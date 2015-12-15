0738104cdacac03c27a8f97d3b5e015db61f3de7
Julien Lacroix <julienlucien.lacroix@gmail.com>
2015-12-15 13:18:43 +0100

    Initialize iOS project

---
+++ ios/Who's Next/classes/LoginController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Who's Next
+//
+//  Created by Julien Lacroix on 15/12/2015.
+//  Copyright © 2015 Mobile Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+#pragma mark - Login Controller: Public interface
+
+@interface LoginController : UIViewController
+
+@end
---
+++ ios/Who's Next/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Who's Next
+//
+//  Created by Julien Lacroix on 15/12/2015.
+//  Copyright © 2015 Mobile Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
