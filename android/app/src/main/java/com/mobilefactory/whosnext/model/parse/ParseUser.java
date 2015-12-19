c56473de6d152dab4746ed5ef963a96aa6d03e0d
Maxime Lumeau <maxime.lumeau@gmail.com>
2015-12-19 22:01:15 +0100

    [Android] fix: ParseUser query

--- android/app/src/main/java/com/mobilefactory/whosnext/model/parse/ParseUser.java
+++ android/app/src/main/java/com/mobilefactory/whosnext/model/parse/ParseUser.java
@@ -2,14 +2,13 @@ package com.mobilefactory.whosnext.model.parse;
 
 import com.mobilefactory.whosnext.model.User;
 import com.parse.ParseClassName;
-import com.parse.ParseObject;
 
 /**
  * Created by Maxime on 19/12/2015.
  */
 
-@ParseClassName("User")
-public class ParseUser extends ParseObject implements User {
+@ParseClassName("_User")
+public class ParseUser extends com.parse.ParseUser implements User {
 
     public ParseUser(){
 
