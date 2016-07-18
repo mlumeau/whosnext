82c31d0b6e7a42d782ac1137dd54a7e632b559f3
Remy Sanfeliu <remy.san@hotmail.com>
2016-07-18 16:20:47 +0200

    [Xamarin] Adding a Log util class

---
+++ xamarin/whosnext/Utils/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace whosnext.Utils
+{
+	public static class Log
+	{
+		public static void debug(string group, string mess)
+		{
+			Console.WriteLine($"[DEBUG] - [{group}] {mess}");
+		}
+
+		public static void info(string group, string mess)
+		{
+			Console.WriteLine($"[INFO]  - [{group}] {mess}");
+		}
+
+		public static void warning(string group, string mess)
+		{
+			Console.WriteLine($"[WARN]  - [{group}] {mess}");
+		}
+
+		public static void error(string group, string mess)
+		{
+			Console.WriteLine($"[ERR]   - [{group}] {mess}");
+		}
+
+		public static void fatal(string group, string mess)
+		{
+			Console.WriteLine($"[FATAL] - [{group}] {mess}");
+		}
+	}
+}
+
